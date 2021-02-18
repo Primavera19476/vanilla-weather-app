5e9f61d796ab433008ce6eef2f46f4730b8cfb00
Primavera19476 <78546579+Primavera19476@users.noreply.github.com>
2021-02-18 21:12:42 +0100

    JS - all Main Elements except current time & Date are now real Data

--- src/app.js
+++ src/app.js
@@ -6,3 +6,24 @@ precipitation
 weather description
 and weather icon are mandatory.
 The forecast is optional */
+
+function displayWeatherMain (response) {
+  console.log (response);
+  let temperatureElementMain = document.querySelector ("#temperature-Main");
+  temperatureElementMain.innerHTML = Math.round(response.data.main.temp);
+  document.querySelector("#current-City").innerHTML = response.data.name;
+  document.querySelector("#description").innerHTML = response.data.weather[0].description;
+  document.querySelector("#feels-like").innerHTML = Math.round(response.data.main.feels_like);
+  document.querySelector("#humidity").innerHTML = Math.round(response.data.main.humidity);
+  document.querySelector("#windspeed").innerHTML = Math.round(response.data.wind.speed);
+  
+}
+
+let apiKey = "e43b0a6cd655b887c6853a81917a0cda";
+let unit = "metric";
+let city = "Paris";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=${unit}&appid=${apiKey}`;
+
+console.log (apiUrl);
+
+axios.get(apiUrl).then(displayWeatherMain);
